b87b7644b48b23ceabdaaf5f03c5f991009cdf0e
Jerry-Hung <37008151+Jerry-Hung@users.noreply.github.com>
2018-03-25 02:40:50 -0700

    Update Practice.scala

--- src/main/scala/pioneer/practice/Practice.scala
+++ src/main/scala/pioneer/practice/Practice.scala
@@ -16,5 +16,5 @@ object Practice {
     * Returns true if we sleep in. We sleep in if it's not a weekday or
     * we're on vacation.
     */
-  // replace this comment with your implementation of sleepIn
+  def sleepIn(w: Boolean, v: Boolean): Boolean = w == false || v == true
 }
